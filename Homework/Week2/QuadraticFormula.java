f5bdb35605ec2e66554c6619a792b6f7b63a08da
Michael <114317937+thatonecodes@users.noreply.github.com>
2025-02-10 15:12:21 -0500

    add week2, add quadratic formula

---
+++ Homework/Week2/QuadraticFormula.java
@@ -0,0 +1,34 @@
+package Homework.Week2;
+
+import java.util.Scanner;
+
+public class QuadraticFormula{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nInput a value:");
+        double aValue = scanner.nextDouble();
+
+
+        System.out.println("\nInput b value:");
+        double bValue = scanner.nextDouble();
+
+        System.out.println("\nInput c value:");
+        double cValue = scanner.nextDouble();
+
+        scanner.close();
+        double discriminant = Math.pow(bValue, 2) - 4 * aValue * cValue;
+
+        double root1 = (-bValue+Math.sqrt(discriminant))/2*aValue;
+
+        double root2 = (-bValue-Math.sqrt(discriminant))/2*aValue;
+
+        if (Double.isNaN(root1)){
+            System.out.println("\n\nRoot 1 and root 2 is not real.");
+        }else{
+            System.out.println("\n\nROOTS ARE: " + root1 + root2);
+        }
+
+    }
+}
