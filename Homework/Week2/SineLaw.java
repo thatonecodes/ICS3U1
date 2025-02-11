74947b3c49dafb400b8cb220d861b8b411ffae1f
Michael <114317937+thatonecodes@users.noreply.github.com>
2025-02-11 16:35:43 -0500

    fix sinelaw java and finish

--- Homework/Week2/SineLaw.java
+++ Homework/Week2/SineLaw.java
@@ -3,10 +3,11 @@ package Homework.Week2;
 import java.util.Scanner;
 
 public class SineLaw {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nEnter the angle A: ");
+        System.out.println("\nEnter the angle A (in degrees): ");
         double angleA = scanner.nextDouble();
 
         System.out.println("Enter the length of side a: ");
@@ -16,18 +17,29 @@ public class SineLaw {
         double sideB = scanner.nextDouble();
         scanner.close();
 
-        double angleB = Math.asin((Math.sin(Math.toRadians(angleA)) * sideB) / sideA);
-        double height = sideA * Math.sin(sideA);
-        System.out.println(angleB);
-        if (sideB < sideA) {
-            System.out.println("No solution.");
-        } else if (sideB == height) {
-            System.out.println("Right side triangle.");
-        } else if (sideB > height && sideA < sideA) {
-            System.out.println();
-        } else {
-            System.out.println();
-        }
+        double angleB = Math.toDegrees(
+            Math.asin((Math.sin(Math.toRadians(angleA)) * sideB) / sideA)
+        );
 
+        double height = sideA * Math.sin(Math.toRadians(angleA));
+
+        System.out.println("\nCalculated Angle B: " + angleB + " degrees");
+        System.out.println("Calculated Height: " + height);
+
+        if (sideB < height || Double.isNaN(angleB)) {
+            System.out.println("No solution (triangle cannot exist).");
+        } else if (sideB == height) {
+            System.out.println("Right triangle.");
+        } else {
+            double angleBPrime = 180 - angleB;
+            if (angleA + angleBPrime < 180) {
+                System.out.println(
+                    "Second Possible AngleB: " + angleBPrime + " degrees"
+                );
+                System.out.println("Two possible triangles exist.");
+            } else {
+                System.out.println("Only one possible triangle exists.");
+            }
+        }
     }
 }
