bd6c92ebd891e336f564889826055b03e40cd3bc
Michael <114317937+thatonecodes@users.noreply.github.com>
2025-02-11 23:07:44 -0500

    add better singleton useful example

---
+++ Extras/java-design-patterns/Singleton/SingletonInput.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SingletonInput {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    private SingletonInput() {} // prevent instantiation from other classes
+
+    public static String input() {
+        System.out.print("Enter your input: ");
+        return "\033[31m" + scanner.nextLine() + "\033[0m"; //add red escape sequence
+    }
+
+    public static void closeScanner() {
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        String input1 = SingletonInput.input();
+        System.out.println("You have entered first input.");
+        String input2 = SingletonInput.input();
+        System.out.println("You have entered second input.");
+        SingletonInput.closeScanner();
+        System.out.printf("Your inputs were: %s and %s\n", input1, input2);
+    }
+}
