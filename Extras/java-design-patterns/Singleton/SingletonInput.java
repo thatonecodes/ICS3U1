c99ce9ce3e6ff2b94b6284853ef06eddb504a864
Michael <114317937+thatonecodes@users.noreply.github.com>
2025-02-18 14:39:57 -0500

    finish nand gate

--- Extras/java-design-patterns/Singleton/SingletonInput.java
+++ Extras/java-design-patterns/Singleton/SingletonInput.java
@@ -1,3 +1,5 @@
+package Singleton;
+
 import java.util.Scanner;
 
 public class SingletonInput {
---
+++ Homework/Week3/NandGate.java
@@ -0,0 +1,16 @@
+package Homework.Week3;
+import java.util.Scanner;
+
+public class NandGate {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("\nPlease enter the a value(boolean): ");
+       Boolean aValue = scanner.nextBoolean();
+       System.out.println("Please enter the b value(boolean): ");
+       Boolean bValue = scanner.nextBoolean();
+       scanner.close();
+
+       boolean output = !(aValue && bValue);
+       System.out.printf("NANDGate output is: %b", output);
+    }
+}
