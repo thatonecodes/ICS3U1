70f50f347e8ab8c1efde65d6a7b048b72702c21e
Michael <114317937+thatonecodes@users.noreply.github.com>
2025-02-20 19:43:07 -0500

    finish homework and boolean assignment

---
+++ Assignments/BooleanTestAssignment/BooleanTest.java
@@ -0,0 +1,26 @@
+package Assignments.BooleanTestAssignment;
+
+import java.util.Scanner;
+
+public class BooleanTest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nPlease enter the a value(boolean): ");
+        Boolean aValue = scanner.nextBoolean();
+        System.out.println("Please enter the b value(boolean): ");
+        Boolean bValue = scanner.nextBoolean();
+        System.out.println("Please enter the c value(boolean): ");
+        Boolean cValue = scanner.nextBoolean();
+        System.out.println("Please enter the d value(boolean): ");
+        Boolean dValue = scanner.nextBoolean();
+        scanner.close();
+
+        Boolean output =
+            (bValue && !dValue) ||
+            (!bValue && !cValue && dValue) ||
+            (!aValue && !bValue && !cValue) ||
+            (aValue && cValue && !dValue);
+        System.out.printf("Your output is: %b", output);
+    }
+}
---
+++ Homework/Week3/NewQuadraticFormula.java
@@ -0,0 +1,33 @@
+package Homework.Week3;
+
+import java.util.Scanner;
+
+public class NewQuadraticFormula {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nInput a value:");
+        double aValue = scanner.nextDouble();
+
+        System.out.println("\nInput b value:");
+        double bValue = scanner.nextDouble();
+
+        System.out.println("\nInput c value:");
+        double cValue = scanner.nextDouble();
+
+        scanner.close();
+        double discriminant = Math.pow(bValue, 2) - 4 * aValue * cValue;
+
+        double root1 = ((-bValue + Math.sqrt(discriminant)) / 2) * aValue;
+        double root2 = ((-bValue - Math.sqrt(discriminant)) / 2) * aValue;
+
+        if (discriminant < 0) {
+            System.out.println("\n\nRoot 1 and root 2 is not real.");
+        } else if (discriminant == 0) {
+            System.out.println("\n\nThere is exactly one real root: " + root1);
+        } else {
+            System.out.println("\n\nROOTS ARE: " + root1 + root2);
+        }
+    }
+}
