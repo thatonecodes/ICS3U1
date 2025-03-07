40270bd4bc352990b65935c928b129d66dc6871f
Maher <114317937+thatonecodes@users.noreply.github.com>
2025-03-07 11:52:24 -0500

    Update AmbiguousCase.java

--- Assignments/AmbigousCaseAssignment/AmbiguousCase.java
+++ Assignments/AmbigousCaseAssignment/AmbiguousCase.java
@@ -19,20 +19,20 @@ public class AmbiguousCase {
 
         double h = b * Math.sin(Math.toRadians(angle));
 
-        if (angle <= 90){
+        if (angle <= 90) {
             if (a < h && a > b) {
                 System.out.println("No solution (triangle cannot exist).");
-            } else if (a > b){
+            } else if (a > b) {
                 System.out.println("There is one possible triangle (obtuse).");
-            }else if (b == h) {
+            } else if (b == h) {
                 System.out.println("Right triangle.");
             } else {
                 System.out.println("There are two possible triangles (ambiguous case).");
             }
-        }else{
-            if (a > b){
+        }else {
+            if (a > b) {
                 System.out.println("One possible triangle.");
-            }else{
+            } else {
                 System.out.println("There are two possible triangles.");
             }
         }
