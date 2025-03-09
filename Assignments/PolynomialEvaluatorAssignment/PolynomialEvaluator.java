bcf37044555e3a52236f3edc5facdc6831d2615a
Michael <114317937+thatonecodes@users.noreply.github.com>
2025-03-08 20:22:11 -0500

    optmimize the polynomialevaluator.java

--- Assignments/PolynomialEvaluatorAssignment/PolynomialEvaluator.java
+++ Assignments/PolynomialEvaluatorAssignment/PolynomialEvaluator.java
@@ -1,78 +1,45 @@
 package Assignments.PolynomialEvaluatorAssignment;
-
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class PolynomialEvaluator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("\nEnter a polynomial in the form f(x)=3x^3 -5x^2 +1x^4 +9x^6 +3.1x^1 +2: ");
-        String polynomial = scanner.nextLine().replace("f(x)=", "").replaceAll("\\s+", ""); // Remove 'f(x)=' and spaces
-
+        String polynomial = scanner.nextLine().replace("f(x)=", "").replaceAll("\\s+", "");
         System.out.println("Enter an x value: ");
-
-        if (!scanner.hasNextDouble()){
-            System.out.println("Invalid x value!");
-            System.exit(1);
-        } 
         double x = scanner.nextDouble();
         scanner.close();
 
-        //true to RETURN THE DELIMITERS FROM TOKEN
-        //see: https://docs.oracle.com/javase/8/docs/api/java/util/StringTokenizer.html#StringTokenizer-java.lang.String-java.lang.String-boolean-
-        StringTokenizer tokenizer = new StringTokenizer(polynomial, "+-", true); 
+        //returnDelims: https://docs.oracle.com/javase/8/docs/api/java/util/StringTokenizer.html#StringTokenizer-java.lang.String-java.lang.String-boolean-
+        StringTokenizer tokenizer = new StringTokenizer(polynomial, "+-", true);
         double result = 0;
         String sign = "+";
-
+        
         while (tokenizer.hasMoreTokens()) {
-            String token = tokenizer.nextToken().trim(); //trim any whitespace
+            String token = tokenizer.nextToken();
             
             if (token.equals("+") || token.equals("-")) {
-                sign = token; //now loop through and see if token is ONE of the signs, set it to the global sign
+                sign = token;
                 continue;
             }
-
-            double coefficient = 1.0;
-            double exponent = 0.0;
-
+            
+            double coef = 1.0;
+            double exp = 0.0;
+            
             if (token.contains("x")) {
-                int xIndex = token.indexOf("x");
-                if (xIndex > 0) { 
-                    try {
-                        coefficient = Double.parseDouble(token.substring(0, xIndex));
-                    } catch (NumberFormatException e) {
-                        System.err.println("You cannot provide a letter as a coefficient! (coeff. parsing err)");
-                        System.exit(1);
-                    }
+                int xPos = token.indexOf("x");
+                if (xPos > 0){
+                    coef = Double.parseDouble(token.substring(0, xPos));
                 }
-
-                if (token.contains("^")) {
-                    try {
-                        exponent = Double.parseDouble(token.substring(token.indexOf("^") + 1));
-                    } catch (NumberFormatException e) {
-                        System.err.println("You cannot provide a letter as an exponent! (exponent parsing err)");
-                        System.exit(1);
-                    }
-                } else {
-                    exponent = 1.0; 
-                }
+                exp = token.contains("^") ? Double.parseDouble(token.substring(token.indexOf("^") + 1)) : 1.0;
             } else {
-                try {
-                    coefficient = Double.parseDouble(token);
-                } catch (NumberFormatException e) {
-                    System.err.println("You cannot provide a letter as a coefficient! (coeff. parsing err)");
-                    System.exit(1);
-                }
-                exponent = 0.0;
+                coef = Double.parseDouble(token);
             }
-
-            if (sign.equals("-")) {
-                coefficient = -coefficient;
-            }
-
-            result += coefficient * Math.pow(x, exponent);
+            
+            result += (sign.equals("-") ? -coef : coef) * Math.pow(x, exp);
         }
-        System.out.printf("f(%.1f) = %.2f%n", x, result); //do %n to remove % cursor
+        
+        System.out.printf("f(%.1f) = %.2f%n", x, result);
     }
 }
