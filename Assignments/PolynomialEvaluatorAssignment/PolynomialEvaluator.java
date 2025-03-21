cdb167b8a5329e01b9c2654d74c54e6a5e51c1b5
Michael <114317937+thatonecodes@users.noreply.github.com>
2025-03-21 19:29:23 -0400

    finish polynomial evaluator

--- Assignments/PolynomialEvaluatorAssignment/PolynomialEvaluator.java
+++ Assignments/PolynomialEvaluatorAssignment/PolynomialEvaluator.java
@@ -8,8 +8,8 @@ public class PolynomialEvaluator {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nEnter a polynomial in the form f(x)=3x^3 -5x^2 +1x^4 +9x^6 +3.1x^1 +2: ");
-        // true to RETURN THE DELIMITERS FROM TOKEN
-        // see:
+        // Using StringTokenizer to split by "+/-" while keeping delimiters
+        // Docs:
         // https://docs.oracle.com/javase/8/docs/api/java/util/StringTokenizer.html#StringTokenizer-java.lang.String-java.lang.String-boolean-
         StringTokenizer tokenizer = new StringTokenizer(scanner.nextLine().replace("f(x)=", "").replaceAll("\\s+", ""),
                 "+-", true);
@@ -18,36 +18,29 @@ public class PolynomialEvaluator {
         double x = scanner.nextDouble();
         scanner.close();
 
-        double result = 0;
+        double result = 0.0;
         String sign = "+";
 
         while (tokenizer.hasMoreTokens()) {
             String token = tokenizer.nextToken();
 
             if (token.equals("+") || token.equals("-")) {
-                sign = token; // set it to the global sign
+                sign = token;
                 continue;
             }
 
             double coeff = 1.0;
             double exp = 0.0;
 
-            if (token.contains("x")) {
-                int xIndex = token.indexOf("x");
-                if (xIndex > 0) {
-                    coeff = Double.parseDouble(token.substring(0, xIndex));
-                }
+            int xIndex = token.indexOf("x");
+            if (xIndex != -1) {
+                coeff = xIndex > 0 ? Double.parseDouble(token.substring(0, xIndex)) : 1.0;
                 exp = token.contains("^") ? Double.parseDouble(token.substring(token.indexOf("^") + 1)) : 1.0;
             } else {
                 coeff = Double.parseDouble(token);
-                exp = 0.0;
             }
 
-            if (sign.equals("-")) {
-                coeff = -coeff;
-            }
-
-            result += coeff * Math.pow(x, exp);
+            result += (sign.equals("-") ? -coeff : coeff) * Math.pow(x, exp);
         }
         System.out.printf("f(%.1f) = %.2f%n", x, result); // do %n to remove % cursor
     }
