3a4151a0fca13337771ec59aa212eba223933ee8
Michael <114317937+thatonecodes@users.noreply.github.com>
2025-03-25 06:26:41 -0400

    make powerrule.java more readable

--- Assignments/PolynomialFunctionAssignment/PowerRule.java
+++ Assignments/PolynomialFunctionAssignment/PowerRule.java
@@ -21,23 +21,28 @@ public class PowerRule {
 
         for (int i = 0; i < coeffs.length; i++) {
             int degree = Integer.parseInt(degrees[i]);
-            if (degree == 0)
+            if (degree == 0) {
                 continue;
+            }
 
             double c1 = Double.parseDouble(coeffs[i]) * degree;
 
             // First derivative
             if (c1 != 0) { // append each coefficient with correct sign then format exponent if degree > 1  
-                deriv1 += (c1 < 0 ? " - " : deriv1.isEmpty() ? "" : " + ") + Math.abs(c1)
-                        + ((degree > 1) ? "x" + ((degree > 2) ? "^" + (degree - 1) : "") : "");
+                String sign = (c1 < 0 ? " - " : deriv1.isEmpty() ? "" : " + ");
+                String exp = ((degree > 1) ? "x" + ((degree > 2) ? "^" + (degree - 1) : "") : "");
+                // formula for derivative 1
+                deriv1 += sign + Math.abs(c1) + exp;
             }
 
             // Second derivative
             if (degree > 1) {
                 double c2 = c1 * (degree - 1);
                 if (c2 != 0) { // append each coefficient with correct sign then format exponent if degree > 2  
-                    deriv2 += (c2 < 0 ? " - " : deriv2.isEmpty() ? "" : " + ") + Math.abs(c2)
-                            + (degree > 2 ? "x" + (degree > 3 ? "^" + (degree - 2) : "") : "");
+                    String sign = (c2 < 0 ? " - " : deriv2.isEmpty() ? "" : " + ");
+                    String exp = (degree > 2 ? "x" + (degree > 3 ? "^" + (degree - 2) : "") : "");
+                    // formula for derivative 2
+                    deriv2 += sign + Math.abs(c2) + exp;
                 }
             }
         }
