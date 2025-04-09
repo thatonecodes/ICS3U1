fa4582172be60aae103412d30988b84c1d0bd4e5
Maher <114317937+thatonecodes@users.noreply.github.com>
2025-04-08 23:20:46 -0400

    small optimisation to dictionary.java

--- Assignments/DictionaryAssignment/Dictionary.java
+++ Assignments/DictionaryAssignment/Dictionary.java
@@ -10,33 +10,33 @@ import java.util.Scanner;
 
 public class Dictionary {
     public static HashSet<String> readDictionary(String fileName) {
-        HashSet<String> dictionary = new HashSet<>();
+        HashSet<String> dict = new HashSet<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                dictionary.add(line);
+                dict.add(line);
             }
         } catch (FileNotFoundException e) {
             System.out.println("Error: File not found!");
         } catch (IOException e) {
             System.out.println("Error reading the file: " + e.getMessage());
         }
-        return dictionary;
+        return dict;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // make filepath interchangeable with different operating systems
-        String fileName = Path.of("Assignments", "DictionaryAssignment", "dictionary.txt").toString();
-        HashSet<String> dictionary = readDictionary(fileName);
+        String filePath = Path.of("Assignments", "DictionaryAssignment", "dictionary.txt").toString();
+        HashSet<String> dictionary = readDictionary(filePath);
 
         System.out.println("\n\nPlease input an english sentence: ");
-        String[] words = scanner.nextLine().trim().replaceAll("\\s+", " ").split(" ");
+        String[] words = scanner.nextLine().trim().split("\\s+");
+        scanner.close();
 
         for (int i = 0; i < words.length; i++) {
             System.out.println(i + 1 + ". " + words[i]
                     + (dictionary.contains(words[i].toLowerCase()) ? " <valid>" : " <invalid>"));
         }
-        scanner.close();
     }
 }
